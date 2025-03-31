691c6dd341e6488a4f76b90645cfe0e4d2efb985
Ian Chipperfield <ianchipperfield@gmail.com>
2025-03-30 18:58:16 -0700

    chore: cleanup action

--- wizardverse-app/vite.config.ts
+++ wizardverse-app/vite.config.ts
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "./",
-  css: {
-    transformer: "lightningcss",
-  },
-  build: {
-    cssMinify: "lightningcss",
-  },
-  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true })],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "dev" ? "" : "/wizardverse/",
+    css: {
+      transformer: "lightningcss",
+    },
+    build: {
+      cssMinify: "lightningcss",
+    },
+    plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true })],
+  };
 });
