42c3a166a6f755f59b763609e0b76cced0daba3f
Ian Chipperfield <ianchipperfield@gmail.com>
2025-03-30 18:49:57 -0700

    fix: base url hopefully

--- wizardverse-app/src/main.tsx
+++ wizardverse-app/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import "./styles.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ basepath: import.meta.env.BASE_URL, routeTree });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
