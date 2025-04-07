fa93d302758229b80de26dfb19fdde4cfded27b9
Andrew Gormley <andrewjgormley@gmail.com>
2025-04-06 23:56:20 -0700

    Sweetass bsky link for main posts

--- wizardverse-app/src/components/Post.tsx
+++ wizardverse-app/src/components/Post.tsx
@@ -1,5 +1,6 @@
 import type { Post } from "../storedPosts";
 import classes from "./Post.module.css";
+import butterfly from "../assets/images/blubutterfly.gif";
 
 type Props = {
   post: Post;
@@ -18,11 +19,18 @@ type Props = {
 const urlForHandle = (handle: string) => `https://bsky.app/profile/${handle}`;
 
 export function Post({ post }: Props) {
-  const { author, text, embed } = post.post;
+  const { author, text, embed, uri } = post.post;
   const images = embed?.images;
+  console.log("Post URI: ", uri);
+  const formattedUri = uri
+    .replace('at://', 'https://bsky.app/profile/')
+    .replace('/app.bsky.feed.post/', '/post/');
   // const formattedText = text.replace(/<br\s*\\?>/g, "\r\n");
   return (
     <div className={classes.post}>
+      <a href={formattedUri} target="_blank" rel="noopener noreferrer">
+        <img className={classes["top-right-image"]} src={butterfly} alt="Bluesky" />
+      </a>
       <img className={classes.avatar} src={author.avatar} />
       <div className={classes.content}>
       <div className={classes.header}>
